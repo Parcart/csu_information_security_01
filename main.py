bc0a7ff2eb14dbcf0ffdfd429332c7c4a77fa1c6
Parfenov Maksim <makcc2992@gmail.com>
2024-12-28 03:56:05 +0500

    del main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame(columns=["label", "message"])
-with open("SMSSpamCollection", 'r') as file:
-    for line in file.readlines():
-        parts = line.strip().split()
-        df = pd.concat([df, pd.DataFrame([{"label": parts[0], "message": ' '.join(parts[1:])}], index=[0])], ignore_index=True)
-
-
